9bfcaf524018ee06e145e0a74b77eca6b198ea13
DivinorWieldor <bilgehan_cagiltay@yahoo.com>
2022-12-15 13:16:42 +0300

    LinghtingShader update
    -removed ambient material property as it is controlled more by diffuse
    +added Textures to reflective objects (diffuse map)

--- Source.cpp
+++ Source.cpp
@@ -276,10 +276,10 @@ int main(){
         lightShader.setVec3("light.ambient", ambientColor);
         lightShader.setVec3("light.diffuse", diffuseColor);
         lightShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
-        lightShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
         lightShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
         lightShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
         lightShader.setFloat("material.shininess", 32.0f);
+        lightShader.setInt("material.diffuse", 1);
 
         // projection transformation
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
--- lightingShader.fs
+++ lightingShader.fs
@@ -3,9 +3,7 @@
 struct Material {
     vec3 specular;//reflection intensity
     float shininess;//metalicity
-    //color of the object
-    vec3 ambient;
-    vec3 diffuse;
+    sampler2D  diffuse;//color + texture of the object
 }; 
 uniform Material material;
 
@@ -20,6 +18,7 @@ uniform Light light;
 
 out vec4 FragColor;
 
+in vec2 TexCoords;
 in vec3 Normal;
 in vec3 FragPos;
 
@@ -27,13 +26,13 @@ uniform vec3 viewPos;
 
 void main(){
     // ambient
-    vec3 ambient = light.ambient * material.ambient;
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
   	
     // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords)); 
 
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
