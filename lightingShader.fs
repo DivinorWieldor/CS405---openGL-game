b2249e77523d5e6d923575e5b0a73c785ea9f10a
DivinorWieldor <bilgehan_cagiltay@yahoo.com>
2022-12-15 03:47:32 +0300

    Updated lightingShader
    +modules are now more modular and parameters can now be set from main()

--- lightingShader.fs
+++ lightingShader.fs
@@ -1,39 +1,45 @@
 #version 330 core
+
 struct Material {
+    vec3 specular;//reflection intensity
+    float shininess;//metalicity
+    //color of the object
     vec3 ambient;
     vec3 diffuse;
-    vec3 specular;
-    float shininess;
 }; 
 uniform Material material;
+
+struct Light {
+    vec3 position;//position of the light
+
+    vec3 ambient;//generally set to a low value
+    vec3 diffuse;//exact color we want light to have (often bright white)
+    vec3 specular;//usually kept at 1 -> shining at full intensity
+};
+uniform Light light;
 
 out vec4 FragColor;
 
 in vec3 Normal;
 in vec3 FragPos;
 
-uniform vec3 lightPos;
-uniform vec3 lightColor;
-uniform vec3 objectColor;
 uniform vec3 viewPos;
 
 void main(){
     // ambient
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient = light.ambient * material.ambient;
   	
     // diffuse 
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
     // specular
-    float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);
             
-    FragColor = vec4((ambient + diffuse + specular) * objectColor, 1.0);
+    FragColor = vec4((ambient + diffuse + specular), 1.0);
 } 
